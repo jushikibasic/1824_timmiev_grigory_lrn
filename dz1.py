24894552f3077a39898950d6b0c532f04b32548d
Jushiki <jushikibasic@gmail.com>
2021-10-02 19:41:32 +0300

    fix of mistakes dz1

--- dz1.py
+++ dz1.py
@@ -2,12 +2,11 @@ duration = int(input('Введите продолжительность врем
 result = ('Итоговое время: ')
 time = {"day": duration // 86400, "hour": duration // 3600 % 24, "min": duration // 60 % 60, "sek": duration % 60}
 if time["day"] > 0:
-    result = result + str(time["day"]) +' дн '
-if time["hour"] > 0:
-    result = result + str(time["hour"]) + ' час '
-if time["min"] >= 0:
-    result = result + str(time["min"]) + ' мин '
-if time["sek"] >= 0:
-
-    result = result + str(time["sek"]) + ' c '
+    result = result + str(time["day"]) +' дн ' + str(time["hour"]) + ' час ' + str(time["min"]) + ' мин ' + str(time["sek"]) + ' cек '
+elif time["hour"] > 0:
+    result = result + str(time["hour"]) + ' час ' + str(time["min"]) + ' мин ' + str(time["sek"]) + ' cек '
+elif time["min"] > 0:
+    result = result + str(time["min"]) + ' мин ' + str(time["sek"]) + ' cек '
+elif time["sek"] > 0:
+    result = result + str(time["sek"]) + ' cек '
 print(result)
