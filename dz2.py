38fbb6ab3ff7ed9ec33cbfa1ca9d69a422b4f8b6
Jushiki <jushikibasic@gmail.com>
2021-10-03 16:54:29 +0300

    refactoring dz2

--- dz2.py
+++ dz2.py
@@ -12,22 +12,20 @@ for value in data:
     summ = 0
     count = value
     while count > 0:
-        digit = count % 10
-        summ += digit
+        summ += count % 10
         count = count // 10
     if summ % 7 == 0:
-        result1 = result1 + value
+        result1 += value
 print(result1)
 print("Задание б/c):")
 for numb in range(len(data)):
-    data[numb] = data[numb] +17
+    data[numb] += 17
 for value in data:
     summ = 0
     count = value
     while count > 0:
-        digit = count % 10
-        summ += digit
+        summ += count % 10
         count = count // 10
     if summ % 7 == 0:
-        result2 = result2 + value
+        result2 += value
 print(result2)
