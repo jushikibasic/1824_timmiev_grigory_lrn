a75da7aa050433df2d8385336fcadbc1f216799a
Jushiki <jushikibasic@gmail.com>
2021-10-03 18:09:40 +0300

    refactoring dz3

--- dz3.py
+++ dz3.py
@@ -1,9 +1,7 @@
 #3
 numb = 1
 while numb < 101:
-    if numb % 10 == 0 or numb % 10 in range(5,10):
-        print(numb, 'процентов')
-    elif 10 < numb < 15:
+    if numb % 10 == 0 or numb % 10 in range(5, 10) or 10 < numb < 15:
         print(numb, 'процентов')
     elif numb % 10 == 1:
         print(numb, 'процент')
