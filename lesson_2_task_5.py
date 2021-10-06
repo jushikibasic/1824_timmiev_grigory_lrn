07e524b8a029dadeb15866a614ac23937301e25e
Jushiki <jushikibasic@gmail.com>
2021-10-06 21:50:28 +0300

    refactoring lesson 2 dz 5

--- lesson_2_task_5.py
+++ lesson_2_task_5.py
@@ -1,7 +1,7 @@
 def list_mask(list_name):
     result = ''
     for price in list_name:
-        result += f'{int(price):d} руб {int(round((price - int(price)) * 100)):02d} коп, '
+        result += f'{int(price)} руб {int(round((price - int(price)) * 100)):02d} коп, '
     return result
 prices = [844.32, 1523.54, 467.17, 235.85, 614.72, 1436.4, 651.85, 1625.0, 321.81, 452.03]
 print(list_mask(prices), 'ID списка: ', id(prices))
