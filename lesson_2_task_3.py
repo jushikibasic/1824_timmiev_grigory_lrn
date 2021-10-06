a4561c87835a53ca9b45bb5c56a8ad2189eb1e74
Jushiki <jushikibasic@gmail.com>
2021-10-06 17:01:27 +0300

    refactoring lesson 2 dz 2 and 3

--- lesson_2_task_3.py
+++ lesson_2_task_3.py
@@ -8,17 +8,16 @@ for value in task_list:
     if ord(value[-1]) in range(48, 58):
         task_list.insert((task_list.index(value) + 1), '"')
 for value in task_list:
-    if ord(value[0]) == 43 or ord(value[0]) == 45:
-        new = int(value[1:])
-        new_value = '{:02d}'.format(new)
-        index = task_list.index(value)
-        task_list[index] = value[:1] + new_value
-for value in task_list:
     if ord(value[0]) in range(48, 58):
         new = int(value)
         new_value = '{:02d}'.format(new)
         index = task_list.index(value)
         task_list[index] = new_value
+    if ord(value[0]) == 43 or ord(value[0]) == 45:
+        new = int(value[1:])
+        new_value = '{:02d}'.format(new)
+        index = task_list.index(value)
+        task_list[index] = value[:1] + new_value
 for name in range(len(task_list)):
     new_name = task_list[name]
     if name == len(task_list) - 1:
