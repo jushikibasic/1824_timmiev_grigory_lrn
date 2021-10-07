0d8a6fd1b33c044ad4a61bb13a125fff5d2fc43a
Jushiki <jushikibasic@gmail.com>
2021-10-08 01:18:28 +0300

    ref lesson 3 task 1

--- Timmiev_Grigory_lesson_3/lesson_3_task_1.py
+++ Timmiev_Grigory_lesson_3/lesson_3_task_1.py
@@ -1,12 +1,12 @@
-def adv_translate(numb: str) -> None:
+def num_translate(num: str) -> None:
     """переводит числительное с английского на русский """
     translate_dict = {
         'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре',
         'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять'
     }
-    numb = numb.lower()
-    if numb in translate_dict:
-        print(numb, "переводится как:", (translate_dict[numb]))
+    num = num.lower()
+    if num in translate_dict:
+        print(num, "переводится как:", (translate_dict[num]))
 
 
-adv_translate(input('Введите английское числительное: '))
+num_translate(input('Введите английское числительное: '))
