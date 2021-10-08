472fdfcf2371436dcb83adfa570a10bce27c8159
Jushiki <jushikibasic@gmail.com>
2021-10-09 01:54:04 +0300

    lesson 3 task 4

--- Timmiev_Grigory_lesson_3/lesson_3_task_4.py
+++ Timmiev_Grigory_lesson_3/lesson_3_task_4.py
@@ -1,27 +1,24 @@
-thesaurus_adv = ("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
+def thesaurus_adv(*args: str) -> dict:
+    """
+    Принимает на вход имена и фамилии и создает из них словарь с ключем по первой букве фамилии
+    и вложенный словарь с ключем по первой букве имени
+    """
+    names_dictionary = {}
+    for name in args:
+        if (name.split()[-1])[0] in names_dictionary:
+            if name[0] in names_dictionary[(name.split()[-1])[0]]:
+                old_name = names_dictionary[(name.split()[-1])[0]][name[0]]
+                names_dictionary[(name.split()[-1])[0]][name[0]] = [old_name, name]
+            else:
+                (names_dictionary[(name.split()[-1])[0]]).setdefault(name[0], [name])
+        else:
+            names_dictionary.setdefault((name.split()[-1])[0], {name[0]: name})
+    return names_dictionary
 
-names_dictionary = {}
-for name in thesaurus_adv:
-    if (name.split()[-1])[0] in names_dictionary:
-        if name[0] in names_dictionary[(name.split()[-1])[0]]:
 
-            old_name = names_dictionary[(name.split()[-1])[0]]
-        old_name = names_dictionary[(name.split()[-1])[0]]
-        names_dictionary[(name.split()[-1])[0]] = [*old_name, name]
-
-    else:
-        names_dictionary.setdefault((name.split()[-1])[0], {name[0]: name})
-#    if name[0] in names_dictionary:
-#        old_name = names_dictionary[name[0]]
-#        names_dictionary[name[0]] = [*old_name, name]
-#    else:
-#        names_dictionary.setdefault(name[0], [name])
-print(names_dictionary)
-
-new_dict = {"А": {"П": ["Петр Алексеев"]},
-            "С": {"И": ["Иван Сергеев", "Инна Серова"],
-                  "А": ["Анна Савельева"]
-                  }
-            }
-
-print(new_dict["С"]["И"])
+names_list = ("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
+result_dict = thesaurus_adv(*names_list)
+list_keys = list(result_dict.keys())
+list_keys.sort()
+for i in list_keys:
+    print(f'"{i}": {result_dict[i]}')
