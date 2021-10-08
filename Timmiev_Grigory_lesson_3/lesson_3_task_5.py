f3ecbb8e6017239d7131188fc341ea4b1aaad6ed
Jushiki <jushikibasic@gmail.com>
2021-10-09 02:14:40 +0300

    lesson 3 task 4

--- Timmiev_Grigory_lesson_3/lesson_3_task_5.py
+++ Timmiev_Grigory_lesson_3/lesson_3_task_5.py
@@ -1,21 +1,22 @@
 import random
 
 
-def get_jokes(count_jokes: int, **kwargs) -> None:
+def get_jokes(count_jokes: int, no_copies=1) -> None:
     nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
     adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
     adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-    counts = kwargs.get('counts', 1)
-    if counts > 5:
-        counts = 1
+    thing_jk = []
+    time_jk = []
+    about_thing_jk = []
     for step in range(count_jokes):
-        thing = random.sample(nouns, counts)
-        time = random.sample(adverbs, counts)
-        about = random.sample(adjectives, counts)
-        seq = "{} {} {}".format(thing, time, about)
-        data.append(seq)
-    print(data)
+        thing_jk.append(random.sample(nouns, no_copies))
+        time_jk.append(random.sample(adverbs, no_copies))
+        about_thing_jk.append(random.sample(adjectives, no_copies))
+        print(thing_jk)
+        print(time_jk)
+        print(about_thing_jk)
 
 
-get_jokes(5, counts=4)
-get_jokes(5)
+
+get_jokes(2)
+#get_jokes(5)
