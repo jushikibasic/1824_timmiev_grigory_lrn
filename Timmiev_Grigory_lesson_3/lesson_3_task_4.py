96a84a64bc8a1ee1ec0b8382c616e50f75e0a593
Jushiki <jushikibasic@gmail.com>
2021-10-10 19:54:18 +0300

    minor fix ln 3 tk 4

--- Timmiev_Grigory_lesson_3/lesson_3_task_4.py
+++ Timmiev_Grigory_lesson_3/lesson_3_task_4.py
@@ -8,19 +8,17 @@ def thesaurus_adv(*args: str) -> dict:
         if (name.split()[-1])[0] in names_dictionary:
             if name[0] in names_dictionary[(name.split()[-1])[0]]:
                 old_name = names_dictionary[(name.split()[-1])[0]][name[0]]
-                names_dictionary[(name.split()[-1])[0]][name[0]] = [old_name, name]
+                names_dictionary[(name.split()[-1])[0]][name[0]] = [*old_name, name]
             else:
                 (names_dictionary[(name.split()[-1])[0]]).setdefault(name[0], [name])
         else:
-            names_dictionary.setdefault((name.split()[-1])[0], {name[0]: name})
+            names_dictionary.setdefault((name.split()[-1])[0], {name[0]: [name]})
     return names_dictionary
 
 
-from pprint import pprint
 names_list = ("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
 result_dict = thesaurus_adv(*names_list)
 list_keys = list(result_dict.keys())
 list_keys.sort()
 for i in list_keys:
     print(f'"{i}": {result_dict[i]}')
-pprint(result_dict)
