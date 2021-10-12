587b239bec282c24c100853887b51d7bc1409a14
Jushiki <jushikibasic@gmail.com>
2021-10-12 20:32:34 +0300

    lesson 4 task 4 ref

--- Timmiev_Grigory_lesson_4/lesson_4_task_4.py
+++ Timmiev_Grigory_lesson_4/lesson_4_task_4.py
@@ -1,5 +1,6 @@
 from utils import currency_rates
 
-print(currency_rates("usd"))
-print(currency_rates("eur"))
-print(currency_rates("gbp"))
+print(currency_rates("usd")[0], currency_rates("usd")[1], sep=', ')
+print(currency_rates("eur")[0], currency_rates("eur")[1], sep=', ')
+print(currency_rates("gbp")[0], currency_rates("gbp")[1], sep=', ')
+
