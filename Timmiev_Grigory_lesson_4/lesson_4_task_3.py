9b5affff7391a2035244e1d4e921706e9767fa17
Jushiki <jushikibasic@gmail.com>
2021-10-12 15:11:37 +0300

    lesson 4 task 3

---
+++ Timmiev_Grigory_lesson_4/lesson_4_task_3.py
@@ -0,0 +1,19 @@
+from cbr_parser.utils import extract_data, extract_datetime
+
+
+def currency_rates(code: str):
+    code = code.upper()
+    data = {}
+    ch_code = extract_data("CharCode")
+    val = extract_data("Value")
+    zip_res = zip(ch_code, val)
+    if code not in ch_code:
+        return None
+    for name, value in zip_res:
+        data.setdefault(name, value)
+    new_data = str(data[code]).replace(",", ".")
+    print(extract_datetime())
+    return float(new_data)
+
+
+print(currency_rates(input("введите международный код валюты :")))
