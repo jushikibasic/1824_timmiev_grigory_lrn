999a65d85ccad425eb394fbe526750c8934bafc0
Jushiki <jushikibasic@gmail.com>
2021-10-15 21:22:05 +0300

    task 5 1 ref

--- Timmiev_Grigory_lesson_5/task_5_1.py
+++ Timmiev_Grigory_lesson_5/task_5_1.py
@@ -8,7 +8,7 @@ if inp_data.isdigit():
     inp_data = int(inp_data)
     odd_to_n = odd_nums(inp_data)
     print("odd_to_n, это :", type(odd_to_n))
-    for _ in range(0, inp_data + 2, 2):
+    for _ in range(0, inp_data + 1, 2):  # inp_data + 2 чтоб генератор выдал StopIteration
         print(next(odd_to_n))
 else:
     print("К вводу допускаются только числа")
