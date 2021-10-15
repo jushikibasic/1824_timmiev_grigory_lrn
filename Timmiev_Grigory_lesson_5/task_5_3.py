2094e734d0b04ece5ed4e9a335bc739134e4926c
Jushiki <jushikibasic@gmail.com>
2021-10-15 13:02:23 +0300

    task 5 3

---
+++ Timmiev_Grigory_lesson_5/task_5_3.py
@@ -0,0 +1,18 @@
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей',
+          'Дмитрий', 'Борис', 'Елена', 'Василий',
+          'Николай', 'Юрий', 'Яков', 'Мария'
+          ]
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
+dist = len(tutors)
+if dist > len(klasses):
+    diff = dist - len(klasses)
+    print("Для", diff, "учеников, не хватает классов.")
+    for _ in range(1, diff + 1):
+        klasses.append(None)
+tur_nam_class = ((stud, aud) for (stud, aud) in zip(tutors, klasses))
+print("tur_nam_class это:", type(tur_nam_class))
+for _ in range(0, dist):
+    print(next(tur_nam_class))
+
+
+
