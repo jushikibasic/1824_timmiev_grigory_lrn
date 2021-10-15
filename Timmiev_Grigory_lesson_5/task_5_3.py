16158f1c61be56e109494f27d1fa5c7f1908c0dc
Jushiki <jushikibasic@gmail.com>
2021-10-15 21:26:22 +0300

    task 5 3 ref

--- Timmiev_Grigory_lesson_5/task_5_3.py
+++ Timmiev_Grigory_lesson_5/task_5_3.py
@@ -5,10 +5,10 @@ klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 dist = len(tutors)
 if dist > len(klasses):
     diff = dist - len(klasses)
-    print("Для", diff, "учеников, не хватает классов.")
+    print("Для", diff, "репетиторов, не хватает классов.")
     for _ in range(0, diff):
         klasses.append(None)
-tur_nam_class = ((stud, aud) for (stud, aud) in zip(tutors, klasses))
+tur_nam_class = ((tut, aud) for (tut, aud) in zip(tutors, klasses))
 print("tur_nam_class это:", type(tur_nam_class))
 for _ in range(0, dist):  # dist + 1 чтоб генератор выдал StopIteration
     print(next(tur_nam_class))
