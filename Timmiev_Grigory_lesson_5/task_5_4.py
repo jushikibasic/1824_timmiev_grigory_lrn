7d5f0fcdfd403982bf278e8f0e18459872649b96
Jushiki <jushikibasic@gmail.com>
2021-10-15 19:36:35 +0300

    task 5 4

--- Timmiev_Grigory_lesson_5/task_5_4.py
+++ Timmiev_Grigory_lesson_5/task_5_4.py
@@ -1,4 +1,9 @@
+from time import perf_counter
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+start = perf_counter()
+result_2 = [src[num + 1] for num in range(len(src) - 1) if src[num] < src[num + 1]]
+print(result_2, perf_counter() - start)
+start = perf_counter()
 result = []
 for name in range(len(src)):
     new_name = src[name]
@@ -8,6 +13,4 @@ for name in range(len(src)):
         next_name = src[name + 1]
     if new_name < next_name:
         result.append(next_name)
-print(result)
-#result_2 = [num for num in range(len(src)) if num == len(src) - 1]
-
+print(result, perf_counter() - start)
