b1ce4d17a081a3d24f7e550ddee53c85a0cbed76
Jushiki <jushikibasic@gmail.com>
2021-10-19 22:18:19 +0300

    nginx_logs.txt

--- Timmiev_Grigoriy_lesson_6/task_6_1.py
+++ Timmiev_Grigoriy_lesson_6/task_6_1.py
@@ -1,1 +1,12 @@
+# получить список кортежей вида:
+# (<remote_addr>,<request_type>,<requested_resource>)
+# Например:[('141.138.90.60', 'GET', '/downloads/product_2')]
+
+with open('Chache/nginx_logs.txt', 'r', encoding='utf-8') as fr:
+    result = []
+    for line in fr:
+        string = line.strip()
+        remote_addr, _, _, _, _, request_type, requested_resource, *_ = string.split()
+        result.append(tuple((remote_addr, request_type[1:], requested_resource)))
+        print(result)
 
