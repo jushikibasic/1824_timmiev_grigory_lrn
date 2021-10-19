d0605442d75e43b9838c94380d8ae58782634b82
Jushiki <jushikibasic@gmail.com>
2021-10-19 19:43:14 +0300

    task 6 1

--- Timmiev_Grigoriy_lesson_6/task_6_1.py
+++ Timmiev_Grigoriy_lesson_6/task_6_1.py
@@ -1,11 +1,1 @@
-from utils import send_request
 
-print(*send_request())
-
-#with open('Chache/nginx_logs.txt', 'w', encoding='utf-8') as fw:
-#    fw.write(str([*send_request()]))
-#    # fw.write(*send_request())
-#
-#file_2 = open('Chache/nginx_logs.txt', 'r', encoding='utf-8')
-#print(file_2.readline().strip())
-#
