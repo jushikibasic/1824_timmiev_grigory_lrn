7cb2f0a864d27446fd07dc1786269eb1168b0b79
Jushiki <jushikibasic@gmail.com>
2021-10-19 23:28:19 +0300

    task 6 1/2

--- Timmiev_Grigoriy_lesson_6/task_6_1.py
+++ Timmiev_Grigoriy_lesson_6/task_6_1.py
@@ -1,12 +1,15 @@
-# получить список кортежей вида:
-# (<remote_addr>,<request_type>,<requested_resource>)
-# Например:[('141.138.90.60', 'GET', '/downloads/product_2')]
-
+result = []
 with open('Chache/nginx_logs.txt', 'r', encoding='utf-8') as fr:
-    result = []
     for line in fr:
         string = line.strip()
         remote_addr, _, _, _, _, request_type, requested_resource, *_ = string.split()
         result.append(tuple((remote_addr, request_type[1:], requested_resource)))
-    print(len(result))
-
+    spam_ch = []
+    for item in result:
+        spam_ch.append(item[0])
+    from collections import Counter
+    a = Counter(spam_ch)
+    max_val = max(a.values())
+    spam_addr = list(a.keys())[list(a.values()).index(max_val)]
+    spam = f'адресс:{spam_addr} запросов: {max_val}'
+print(spam)
