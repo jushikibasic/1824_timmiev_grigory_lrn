a218cd54caecedfdb81e8bc94a6467d16fe87a9f
Jushiki <jushikibasic@gmail.com>
2021-10-20 23:35:36 +0300

    task 6 4 ref

--- Timmiev_Grigoriy_lesson_6/task_6_4.py
+++ Timmiev_Grigoriy_lesson_6/task_6_4.py
@@ -2,25 +2,27 @@ import sys
 from itertools import zip_longest
 
 
+def line_to_list(file):
+    data = []
+    for line in file:
+        string = line.strip()
+        data.append(string)
+    return data
+
+
 def user_hobby_fn(file_1, file_2, file_3):
     with open(file_1, 'r', encoding='utf-8') as fr:
-        users = []
-        for line in fr:
-            string = line.strip()
-            users.append(string)
+        users = line_to_list(fr)
     with open(file_2, 'r', encoding='utf-8') as fr:
-        hobby = []
-        for line in fr:
-            string = line.strip()
-            hobby.append(string)
+        hobby = line_to_list(fr)
     if len(users) < len(hobby):
         sys.exit(1)
     else:
         result = dict(zip_longest(users, hobby[:len(users)]))
         with open(file_3, 'w+', encoding='utf-8') as fw:
             for key, val in result.items():
-                ch = f'{key}: {val}\n'
-                fw.write(ch)
+                _ = f'{key}: {val}\n'
+                fw.write(_)
 
 
 users_file = 'Chache/users.csv'
