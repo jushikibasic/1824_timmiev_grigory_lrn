9204f351ee6bc7fcdfbf8bc3084e6bd40bea4ccd
Jushiki <jushikibasic@gmail.com>
2021-10-27 22:02:37 +0300

    Task 8 3

--- Timmiev_Grigory_lesson_8/task_8_3.py
+++ Timmiev_Grigory_lesson_8/task_8_3.py
@@ -1,15 +1,24 @@
-# 3.
-# Написать декоратор для логирования типов позиционных аргументов функции, например:
-# def type_logger...
-# ...
-# @type_logger
-# def calc_cube(x):
-# return x ** 3
-# >>> a = calc_cube(5)
-# 5: <class 'int'>
-# Примечание: если аргументов несколько - выводить данные о каждом через запятую; можете
-# ли вы вывести тип значения функции? Сможете ли решить задачу для именованных
-# аргументов? Сможете ли вы замаскировать работу декоратора? Сможете ли вывести имя
-# функции, например, в виде:
-# >>> a = calc_cube(5)
-# calc_cube(5: <class 'int'>)
+def for_(name, fun):
+    print(f'{fun.__name__}({name}: {type(fun(name))})')
+
+
+def type_logger(func):
+    def abracadabra(*args, **kwargs):
+        result = []
+        for item in args:
+            for_(item, func)
+            result.append(func(item))
+        for _, value in kwargs.items():
+            for_(value, func)
+            result.append(func(value))
+        return result
+    return abracadabra
+
+
+@type_logger
+def calc_cube(x):
+    return x ** 3
+
+
+a = calc_cube(5, 6, g=3)
+print(*a, sep=', ')
