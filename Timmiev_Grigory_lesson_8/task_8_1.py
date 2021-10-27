ea2d2d2ed69ab7edca2e62b6218125ab7171da05
Jushiki <jushikibasic@gmail.com>
2021-10-27 23:27:53 +0300

    Task 8 1 ref

--- Timmiev_Grigory_lesson_8/task_8_1.py
+++ Timmiev_Grigory_lesson_8/task_8_1.py
@@ -6,8 +6,7 @@ RE_MAIL = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9]+)+$')
 def email_parse(email_address):
     if RE_MAIL.match(email_address):
         keys = ['username', 'domain']
-        tmp = email_address.split('@')
-        return dict(zip(keys, tmp))
+        return dict(zip(keys, email_address.split('@')))
     else:
         raise ValueError(f'ValueError: wrong email: {email_address}')
 
