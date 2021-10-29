a79c8ae8633eaa54e4915ddc0017948e4610d9f2
Jushiki <jushikibasic@gmail.com>
2021-10-30 00:24:00 +0300

    task 9 3

--- Timmiev_Grigory_lesson_9/task_9_3.py
+++ Timmiev_Grigory_lesson_9/task_9_3.py
@@ -4,9 +4,10 @@ income_scientist = {'wage': 150000, 'bonus': 25000}
 
 
 class Worker:
-    def __init__(self, name: str, surname: str, income: dict):
+    def __init__(self, name: str, surname: str, position: str, income: dict):
         self.name = name
         self.surname = surname
+        self.position = position
         self._income = income
 
 
@@ -19,9 +20,9 @@ class Position(Worker):
         return sum(self._income.values())
 
 
-welder = Position('Иван', 'Васильев', income_welder)
-driver = Position('Петр', 'Николаев', income_driver)
-scientist = Position('Геннадий', 'Разумов', income_scientist)
+welder = Position('Иван', 'Васильев', 'сварщик', income_welder)
+driver = Position('Петр', 'Николаев', 'водитель', income_driver)
+scientist = Position('Геннадий', 'Разумов', 'ученый', income_scientist)
 print(welder.get_full_name(), welder.get_total_income())
 print(driver.get_full_name(), driver.get_total_income())
 print(scientist.get_full_name(), scientist.get_total_income())
