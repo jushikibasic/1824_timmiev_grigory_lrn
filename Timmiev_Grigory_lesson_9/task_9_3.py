e1dca062e99709874cf9bff677c1db5ded81cafa
Jushiki <jushikibasic@gmail.com>
2021-10-30 11:23:32 +0300

    task 9 3 ref

--- Timmiev_Grigory_lesson_9/task_9_3.py
+++ Timmiev_Grigory_lesson_9/task_9_3.py
@@ -1,14 +1,14 @@
-income_welder = {'wage': 50000, 'bonus': 15000}
-income_driver = {'wage': 30000, 'bonus': 7500}
-income_scientist = {'wage': 150000, 'bonus': 25000}
+income_welder = [50000, 15000]
+income_driver = [30000, 7500]
+income_scientist = [150000, 25000]
 
 
 class Worker:
-    def __init__(self, name: str, surname: str, position: str, income: dict):
+    def __init__(self, name: str, surname: str, position: str, wage: int, bonus: int):
         self.name = name
         self.surname = surname
         self.position = position
-        self._income = income
+        self._income = {"wage": wage, "bonus": bonus}
 
 
 class Position(Worker):
@@ -20,9 +20,9 @@ class Position(Worker):
         return sum(self._income.values())
 
 
-welder = Position('Иван', 'Васильев', 'сварщик', income_welder)
-driver = Position('Петр', 'Николаев', 'водитель', income_driver)
-scientist = Position('Геннадий', 'Разумов', 'ученый', income_scientist)
+welder = Position('Иван', 'Васильев', 'сварщик', *income_welder)
+driver = Position('Петр', 'Николаев', 'водитель', *income_driver)
+scientist = Position('Геннадий', 'Разумов', 'ученый', *income_scientist)
 print(welder.get_full_name(), welder.get_total_income())
 print(driver.get_full_name(), driver.get_total_income())
 print(scientist.get_full_name(), scientist.get_total_income())
