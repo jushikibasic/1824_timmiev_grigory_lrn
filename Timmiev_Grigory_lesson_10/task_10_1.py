c6e0c95d026ec9562eacd39232e5c734445de02b
Jushiki <jushikibasic@gmail.com>
2021-11-02 22:56:44 +0300

    task 10 1

--- Timmiev_Grigory_lesson_10/task_10_1.py
+++ Timmiev_Grigory_lesson_10/task_10_1.py
@@ -1,5 +1,19 @@
 # Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод
 # __init__()), который должен принимать данные (список списков) для формирования матрицы.
+list_1 = [[10, 2, 3], [3, 5, 6]]
+
+print(list_1)
+
 
 class Matrix:
-    def __init__(self):
+    def __init__(self, list_in):
+        for n in range(len(list_in)):
+            list(n)
+            self.line = n
+
+    def show(self):
+        print(self.line)
+
+
+a = Matrix(list_1)
+a.show()
