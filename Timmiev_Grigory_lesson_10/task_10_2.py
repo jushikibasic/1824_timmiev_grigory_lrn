470e4f4df7e5f611e4b8c064a7d8cd6d246a2aaf
Jushiki <jushikibasic@gmail.com>
2021-11-04 00:48:17 +0300

    task 10 2

--- Timmiev_Grigory_lesson_10/task_10_2.py
+++ Timmiev_Grigory_lesson_10/task_10_2.py
@@ -1,22 +1,27 @@
 class Clothes:
+    def __init__(self):
+        self.res = None
+
     def __str__(self):
         return f'расход ткани на {self.__class__.__name__}:\n\t{self.res}'
 
     def __add__(self, other):
         if not isinstance(other, Clothes):
             raise TypeError(f'{other} not  a Clothes class')
-        result = self.res + other.res
+        result = round(self.res + other.res, 3)
         return f'всего ткани потребуется:\n\t{result}'
 
 
 class Coat(Clothes):
     def __init__(self, value):
-        self.res = value / 6.5 + 0.5
+        super(Coat, self).__init__()
+        self.res = round(value / 6.5 + 0.5, 3)
 
 
 class Jacket(Clothes):
     def __init__(self, high):
-        self.res = 2 * high + 0.3
+        super(Jacket, self).__init__()
+        self.res = round(2 * high + 0.3, 3)
 
 
 coat = Coat(10)
