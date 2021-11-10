9b4ca197c6f32b70841a4c0f4817a8f09ec1cbb7
Jushiki <jushikibasic@gmail.com>
2021-11-10 18:46:39 +0300

    task 11 2

--- Timmiev_Grigory_lesson_11/task_11_2.py
+++ Timmiev_Grigory_lesson_11/task_11_2.py
@@ -1,3 +1,14 @@
-# Создать собственный класс-исключение, обрабатывающий ситуацию деления на ноль.
-# Проверить его работу на данных, вводимых пользователем. При вводе нуля в качестве
-# делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+from my_exceptions import ZeroDivExcept
+
+
+def div(a, b):
+    try:
+        if b == 0:
+            raise ZeroDivExcept
+        return a / b
+    except ZeroDivExcept as er:
+        print(er)
+
+
+print(div(4, 0))
+print(div(4, 2))
