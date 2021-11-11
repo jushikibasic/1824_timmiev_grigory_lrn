f5f13488109cfcde977e6660ec275abf9f13bf53
Jushiki <jushikibasic@gmail.com>
2021-11-11 18:27:59 +0300

    task 11 7

--- Timmiev_Grigory_lesson_11/task_11_7.py
+++ Timmiev_Grigory_lesson_11/task_11_7.py
@@ -1,5 +1,44 @@
-# Реализовать проект «Операции с комплексными числами». Создать класс «Комплексное
-# число». Реализовать перегрузку методов сложения и умножения комплексных чисел.
-# Проверить работу проекта. Для этого создать экземпляры класса (комплексные числа),
-# выполнить сложение и умножение созданных экземпляров. Проверить корректность
-# полученного результата.
+from my_exceptions import NotAComplex
+
+
+class ComplexNumb:
+    def __init__(self, math_a: int, math_b):
+        self.math_a = math_a
+        self.math_b = math_b
+
+    @staticmethod
+    def check(other):
+        return isinstance(other, ComplexNumb)
+
+    def __str__(self):
+        return f'{self.math_a} + {self.math_b}*i'
+
+    def __add__(self, other):
+        try:
+            if not ComplexNumb.check(other):
+                raise NotAComplex
+            else:
+                x = self.math_a + other.math_a
+                y = self.math_b + other.math_b
+                return ComplexNumb(x, y)
+        except NotAComplex as er:
+            print(f'{other} {er}')
+
+    def __mul__(self, other):
+        try:
+            if not ComplexNumb.check(other):
+                raise NotAComplex
+            else:
+                x = self.math_a * other.math_a - self.math_b * other.math_b
+                y = self.math_a * other.math_b + self.math_b * other.math_a
+                return ComplexNumb(x, y)
+        except NotAComplex as er:
+            print(f'{other} {er}')
+
+
+a = ComplexNumb(1, 4)
+b = ComplexNumb(6, 8)
+z = a + b
+j = a * b
+print(z)
+print(j)
